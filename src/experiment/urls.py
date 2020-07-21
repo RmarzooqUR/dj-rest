c87b696e8d07e52799d9505b76b57f10d8220316
RmarzooqUR <marzooq697231@gmail.com>
2020-07-22 00:42:54 +0530

    play with get params and post params

--- src/experiment/urls.py
+++ src/experiment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TestView.as_view(), name="test")
+    path('', views.TestView.as_view(), name="test"),
+    path('<slug:param>/', views.TestApiGetView.as_view(), name='paramTest')
 ]
--- src/experiment/views.py
+++ src/experiment/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.utils import timezone
+from .serializers import PostSerializer
+from .models import Post
 
 class TestView(APIView):
-    data = {
-        'name'  :   'marzooq',
-        'time'  :   timezone.now(),
-    }
 
     def get(self, request, *args, **kwargs):
-        return Response(self.data)
+        data = {
+            'name'  :   'marzooq',
+            'time'  :   timezone.now(),
+        }
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serial = PostSerializer(data=request.data)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data)
+        return serial.errors
+
+class TestApiGetView(APIView):
+    def get(self, request, *args, **kwargs):
+        title = kwargs['param']
+        qs = Post.objects.get(title=title)
+        post = PostSerializer(qs)
+        return Response(post.data)
